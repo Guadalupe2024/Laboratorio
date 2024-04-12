76a8af0790e1c8f553fe18de88f53307c3d1ca05
Katia Guadalupe Vásquez Ramírez <163598674+Guadalupe2024@users.noreply.github.com>
2024-04-12 14:01:34 -0300

    Update OSLinux.h

--- OSWINDOWS/include/OSLinux.h
+++ OSWINDOWS/include/OSLinux.h
@@ -1,11 +1,3 @@
-#ifndef OSLINUX_H
-#define OSLINUX_H
-#ifndef OSWINDOWS_H
-#define OSWINDOWS_H
-#ifndef OSOSX_H
-#define OSOSX_H
-#include <OSOSx.h>
-#include <stdio.h>
 #include <iostream>
 using namespace std;
 #include <string.h>
@@ -28,41 +20,5 @@ class OSLinux : public iApariencia
         void DibujarSimbolo(char,int,bool _saltarlinea = false);
         void CentrarTexto(char*);
 };
-
-class OSWindows:public iApariencia{
-    public:
-        OSWindows();
-        virtual ~OSWindows();
-        void PintarWindow();
-
-    protected:
-
-    private:
-        char _win_titulo[100];
-        char _win_area_trabajo[100];
-        char _win_estado[100];
-        void DibujarSimbolo(char,int,bool _saltarlinea=false);
-        void CentrarTexto(char*);
-};
-
-#endif // OSWINDOWS_H
-
-class OSOSx : public iApariencia
-{
-    public:
-        OSOSx();
-        virtual ~OSOSx();
-        void PintarOSOSx();
-
-    protected:
-
-    private:
-        char _osx_titulo[100];
-        char _osx_area_trabajo[100];
-        char _osx_estado[100];
-        void DibujarSimbolo(char,int,bool _saltarlinea = false);
-        void CentrarTexto(char*);
-};
-
 
 #endif // OSLINUX_H
